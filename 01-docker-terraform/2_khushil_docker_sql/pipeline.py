f009f4942a040c8346a2a306225019a0f3cca922
khushil-sketch <khushiln6@gmail.com>
2025-07-25 12:52:14 -0400

    completed building simple docker pipeline

---
+++ 01-docker-terraform/2_khushil_docker_sql/pipeline.py
@@ -0,0 +1,6 @@
+import pandas as pd 
+
+# some fancy pandas code
+
+# at the end of it all, print jb finished - this code just helps to know that the pandas has been imported and that the pipeline is complete
+print("job finished")
