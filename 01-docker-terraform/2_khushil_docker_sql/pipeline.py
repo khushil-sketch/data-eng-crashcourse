b03a3344b1fb3f2b6160f8fe7bcee5a3b628fe66
khushil-sketch <khushiln6@gmail.com>
2025-08-01 19:18:08 -0400

    testing to see if git is now tracking my files after the merge

--- 01-docker-terraform/2_khushil_docker_sql/pipeline.py
+++ 01-docker-terraform/2_khushil_docker_sql/pipeline.py
@@ -3,4 +3,5 @@ import pandas as pd
 # some fancy pandas code
 
 # at the end of it all, print jb finished - this code just helps to know that the pandas has been imported and that the pipeline is complete
+print("meow")
 print("job finished")
